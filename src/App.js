bdb2263a943753113d9a2f2e826df85932ed5da4
ksommi <ksommi@gmail.com>
2022-06-14 12:23:07 -0300

    NavBar - Sommi

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <h1>Home</h1>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <ul>
+      <li>
+        <a href="#">Inicio</a>
+      </li>
+      <li>
+        <a href="#">Indumentaria</a>
+      </li>
+      <li>
+        <a href="#">Pelotas</a>
+      </li>
+      <li>
+        <a href="#">Botines</a>
+      </li>
+      <li>
+        <a href="#">Contacto</a>
+      </li>
+      <li>
+        <a href="#">Iniciar Sesión</a>
+      </li>
+    </ul>
+  );
+};
+
+export default NavBar;
